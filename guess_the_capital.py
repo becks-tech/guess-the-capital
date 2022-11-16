02f2dfb75f5d5e38914a376a0228d499252091a9
Rebecca Shepherd <rebeccads.95@hotmail.com>
2022-11-16 16:58:33 +0000

    Changed made

--- guess_the_capital.py
+++ guess_the_capital.py
@@ -30,24 +30,48 @@ capitals = {"France":"Paris",
                 "India":"New Delhi",
                 "Norway":"Oslo"} #26
 
-#print(capitals)
-
 user_input = 0
+count = 0
+total = 0
 
-r = [random.choice(list(capitals)) for i in range(10)]
-#needs to be printed as a loop
+def main():
+    for i in range(10):
+        r = random.choice(list(capitals))
+    print(r)
 
-print(r)
-
-def guess():
-    #then work on the user input guess 
+    user_input = input("What is the capital of: ", r)
     if user_input in capitals:
+        global count
+        count += 1
         print("Correct!")
+        return count
     else:
         print("Incorrect.")
 
-def main():
-    pass
+#turn above into input
+def score():
+    if total == 10:
+        global result
+        result = count / total
+        global percentage
+        percentage = round((result * 100),2)
+        if percentage >= 75:
+            print("Excellent")
+        elif percentage >=50:
+            print("Good")
+        else:
+            print("Need to improve")
+
+    #print("You answered", total, "questions with" , count "correct")
+    print("Your score is", percentage, "%")
+    return result
+
+    
+
+
+main()
+
+
 
 executionTime = (time.time() - startTime)
 print("Time taken in seconds: " + str(format(executionTime, ".2f")))
